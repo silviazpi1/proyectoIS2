c527b1d3858410204fe18e931fe101dc0bfbdd11
silviazpi1 <silviaromazp+1@gmail.com>
2020-12-18 16:45:18 +0100

    Base de datos actualizada

--- MundoDesconocido/settings.py
+++ MundoDesconocido/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,13 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dr#jch&_z$^^9)t-l=-&f@z8vkk&rsqcxx-w%ls#799s4#p38e'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
@@ -82,9 +85,9 @@ DATABASES = {
         'ENGINE': 'djongo',
         "CLIENT": {
             'name': 'BDSimple',
-            'host': 'mongodb+srv://enrique:ufvpassword@cluster0.vwexh.mongodb.net/BDSimple?retryWrites=true&w=majority',
-            "username": 'enrique',
-            'password': 'ufvpassword',
+            'host': env("BD_HOST"),
+            "username": env("BD_USER_NAME"),
+            'password': env("BD_PASSWORD"),
             'authMechanism': 'SCRAM-SHA-1',
         },
     }
--- MundoDesconocido/urls.py
+++ MundoDesconocido/urls.py
@@ -31,10 +31,3 @@ urlpatterns = [
     path('', home, name='home'),
     path('index/', home, name='home'),
 ]
-
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
