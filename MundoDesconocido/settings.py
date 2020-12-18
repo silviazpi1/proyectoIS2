a9ba13f7f234df1264cddee7065f1aaa899340df
Fernando Perez UFV <fernando.perez.lara+1@outlook.com>
2020-12-18 17:30:43 +0100

    Plantilla creada

--- MundoDesconocido/settings.py
+++ MundoDesconocido/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -131,3 +131,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "templates/")]
