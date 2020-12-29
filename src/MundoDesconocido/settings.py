ead379cd18b3970f10985bc89bec0178faf74bb7
Silvia <silviaromazp+1@gmail.com>
2020-12-29 10:38:45 +0100

    static root added

--- src/MundoDesconocido/settings.py
+++ src/MundoDesconocido/settings.py
@@ -137,6 +137,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "templates/")]
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, '/static/'))
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media') 
 MEDIA_URL = '/media/'
