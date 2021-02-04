e6e640493397d0ecbb47bb158b8a30afad0f226a
Silvia <silviaromazp+1@gmail.com>
2021-02-04 14:53:25 +0100

    Responsive 100%

--- src/templates/js/navigation.js
+++ src/templates/js/navigation.js
@@ -1,5 +1,16 @@
 var navigation = document.getElementsByClassName("navElement");
+var isMenuOpen = false;
 
 function setCurrentPage(index) {
     navigation[index].classList.add("aCurrent");
+}
+
+function toggleMenu() {
+    if (isMenuOpen === false) {
+        document.querySelector("header#hdrMain nav.navMenu").style.display = "block";
+    } else {
+        document.querySelector("header#hdrMain nav.navMenu").style.display = "none";
+    }
+
+    isMenuOpen = !isMenuOpen;
 }
